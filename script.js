30ff53af4934f92c48216759a230498aa39d7a18
akshay_gaikwad <akshay240695@gmail.com>
2023-05-09 22:06:37 +0530

    Changed styling and fixed password merging

--- script.js
+++ script.js
@@ -5,35 +5,24 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 
 function generatePassword() {
 
-  // Trying to figure out how to restrict 'generate passwords' from being activated a second time without resetting the results of the first
-  
-  // if (firstPassword.value != "") {
-  //   passwordLenEl.value = null;
-  //   firstPassword.value = null;
-  //   secondPassword.value = null;
-  // }
-
   let passwordLenEl = document.getElementById("input-Length");
-  // console.log(passwordLenEl.value);
-  // console.log(typeof(passwordLenEl.value));
 
   let firstPassword = document.getElementById("password1");
   let secondPassword = document.getElementById("password2");
 
+  firstPassword.textContent = "";
+  secondPassword.textContent = "";
+
   if (passwordLenEl.value === null || passwordLenEl.value > 15 || passwordLenEl.value < 6) {
     alert("Password must be 6-15 characters long");
-    passwordLenEl.value = ""; 
+    passwordLenEl.textContent = ""; 
   }
 
   for (let i=0 ; i<passwordLenEl.value ; i++) {
-    firstPassword.value += randomChar();
-    secondPassword.value += randomChar();
+    firstPassword.textContent += randomChar();
+    secondPassword.textContent += randomChar();
   }
 
-  // As I have used input elements for storing the generated passwords, disabling the element once the password is generated, thus avoiding accidental addition/deletion of characters by user
-
-  firstPassword.disabled = true;
-  secondPassword.disabled = true;
 }
 
 // function to only obtain a random number
